21b851ad1b0bb02aa9afe039c04beae92a887ffb
life1 <life1@DESKTOP-4ARPRNQ.dhcp.wustl.edu>
2023-04-06 14:00:34 -0500

    We're done

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -2,7 +2,20 @@ package studio8;
 
 public class Date {
 
+	
+	private int month;
+	private int day;
+	private int year;
+	private boolean holiday;
+	
+	public Date(int month, int day, int year) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+	}
 
+	public 
+	
     public static void main(String[] args) {
 
     }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -3,9 +3,33 @@ package studio8;
 
 public class Time {
 
+	private int hour;
+	private int minute;
+	private boolean militaryTime; 
+
+	public Time(int hour, int minute) {
+		this.hour = hour;
+		this.minute = minute;
+	}
+
+	public String getTime()
+	{
+		if (militaryTime == true)
+		{
+			return (hour + ": " + minute);
+		}
+		else 
+		{
+			if (hour>12) {
+				return ((hour-12) + ": " + minute);
+			}
+		}
+		return " ";
+	}
+
 	public static void main(String[] args) {
- 
-    	
-    }
+
+
+	}
 
 }
